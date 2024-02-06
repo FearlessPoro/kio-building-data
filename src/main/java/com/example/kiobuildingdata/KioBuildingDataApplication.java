08f565ddecd6870113a62279aff3f461c443f2d9
FearlessPoro <wojtek.franczuk@gmail.com>
2024-02-06 22:46:11 +0100

    Add Spring Initializer files

---
+++ src/main/java/com/example/kiobuildingdata/KioBuildingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiobuildingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioBuildingDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioBuildingDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kiobuildingdata/KioBuildingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiobuildingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioBuildingDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
